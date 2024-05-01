9f888ec17b3a29897bcd529f5446035d4f9ca886
Paola <Paola@DESKTOP-L53JRFL>
2024-04-30 22:13:22 -0300

    Adicionar arquivos de projeto.

---
+++ 1016_distancia_Study/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+class URI
+{
+    static void Main(string[] args)
+    {
+        //declarar
+        int tempo, distancia;
+
+        // Ler a distância em quilômetros
+        distancia = int.Parse(Console.ReadLine());
+
+        // Multiplicamos essa distância por 2, já que o carro Y se afasta um quilômetro a cada 2 minutos.
+        tempo = distancia * 2 ;
+
+        Console.WriteLine(tempo + " minutos");
+
+        Console.Read();
+
+    }
+
+}
+
+
